228b36c6204e4f9aa940c82c1c52709ced71d3f2
Quinlan Pfiffer <qpfiffer@gmail.com>
2015-06-09 23:15:32 -0700

    WORKS EVERYTIME

--- the-script-downloading-protocol/server.py
+++ the-script-downloading-protocol/server.py
@@ -13,6 +13,7 @@ def main(to_serve):
     except:
         return
     mmapd_file = mmap.mmap(to_serve_file.fileno(), 0, prot = mmap.PROT_READ)
+    to_serve_file.close()
 
     udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_sock.bind((THE_IP_ADDY, THE_PROTOCOL_PORT))
@@ -28,6 +29,7 @@ def main(to_serve):
         print "GOT HI!"
         udp_sock.sendto(gzip_encoded_hi, addr)
 
+        print "Reading from socket bytes."
         readable, _, _ = select.select([udp_sock], [], [], 5000)
         buf = array.array('i', [0]) # The fuck does this work?
         fcntl.ioctl(readable[0].fileno(), termios.FIONREAD, buf, True)
@@ -41,8 +43,13 @@ def main(to_serve):
         print "SSN IS {}".format(zlib.decompress(gzip_encoded_ssn))
 
         print "Sending file!"
-        udp_sock.sendto(zlib.compress(mmapd_file.read(mmapd_file.size())), addr)
+        compressed_file = zlib.compress(mmapd_file.read(mmapd_file.size()))
+        udp_sock.sendto(compressed_file, addr)
+        mmapd_file.seek(0)
 
 if __name__ == '__main__':
     to_serve = argv[1]
+    ssn = argv[2]
+    if not ssn or not to_serve:
+        print "Usage: {} <file_to_serve> <your social security number>".format(argv[0])
     main(to_serve)
